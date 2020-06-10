a5f53d140018a084f047ab02cc3e7ce42463f6be
ang <andy.ng@digicatapult.org.uk>
2020-06-10 22:45:14 +0100

    v1.0

--- scripts/project-bootstrap.sh
+++ scripts/project-bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -e
+
 SUBSCRIPTION=
+TENANT=
 LOCATION=UKSOUTH
 RG_NAME=
 RG_SECRET_NAME=
@@ -48,7 +51,7 @@ echo "Storage account $TF_STATE_STORAGE_ACCOUNT_NAME created."
 
 # Retrieve the storage account key
 echo "Retrieving storage account key..."
-ACCOUNT_KEY=$(az storage account keys list --resource-group $RG_NAME --account-name $TF_STATE_STORAGE_ACCOUNT_NAME --query '[0]'.value -o tsv)
+ACCOUNT_KEY=$(az storage account keys list --resource-group $RG_SECRET_NAME --account-name $TF_STATE_STORAGE_ACCOUNT_NAME --query '[0]'.value -o tsv)
 
 echo "Storage account key retrieved."
 
@@ -61,10 +64,54 @@ az storage container create --name $TF_STATE_CONTAINER_NAME \
 
 # Store the Terraform State Storage Key into KeyVault
 echo "Store storage access key into key vault secret..."
-az keyvault secret set --name angdevst-key --value $ACCOUNT_KEY --vault-name $KEYVAULT_NAME
+az keyvault secret set --name angdevst-key --value $ACCOUNT_KEY --vault-name $KV_NAME
 
 echo "Key vault secret created."
 
 # Generate random string for Service Principal
 SP_SECRET=$(openssl rand -base64 64)
 
+# Create Service Principal with permissions to RG
+echo "Creating $SERVICEPRINCIPAL_NAME Service Principal..."
+az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$SUBSCRIPTION/resourceGroups/$RG_NAME" --name $SERVICEPRINCIPAL_NAME
+
+echo "Service Principal created"
+
+echo "Reset credential..."
+az ad sp credential reset --name $SERVICEPRINCIPAL_NAME --password "$SP_SECRET"
+
+echo "Credentials reset and storing in Key Vault..."
+az keyvault secret set --name "ARM-CLIENT-SECRET" --value "$SP_SECRET" --vault-name $KV_NAME
+
+# Give SP access to KV Secrets
+SP_OBJ_ID=$(az ad sp list --display-name $SERVICEPRINCIPAL_NAME --query "[].{id:objectId}" -o tsv)
+
+echo "Give $SERVICEPRINCIPAL_NAME access to $KV_NAME Key Vault..."
+az keyvault set-policy --name $KV_NAME --object-id "$SP_OBJ_ID" --secret-permissions get list set
+
+echo "KV access applied"
+
+# Give SP access to Container
+echo "Give $SERVICEPRINCIPAL_NAME access to $TF_STATE_CONTAINER_NAME..."
+az role assignment create \
+    --role "Storage Blob Data Contributor" \
+    --assignee-object-id "$SP_OBJ_ID" \
+    --assignee-principal-type ServicePrincipal \
+    --scope "/subscriptions/$SUBSCRIPTION/resourceGroups/$RG_SECRET_NAME/providers/Microsoft.Storage/storageAccounts/$TF_STATE_STORAGE_ACCOUNT_NAME/blobServices/default/containers/$TF_STATE_CONTAINER_NAME"
+
+echo "Container access applied"
+
+# Store .envrc information into Key Vault
+APP_ID=$(az ad sp list --display-name $SERVICEPRINCIPAL_NAME --query [].{id:appId} -o tsv)
+
+az keyvault secret set --name "ARM-CLIENT-ID" --value "$APP_ID" --vault-name $KV_NAME
+az keyvault secret set --name "ARM-SUBSCRIPTION-ID" --value "$SUBSCRIPTION" --vault-name $KV_NAME
+az keyvault secret set --name "ARM-TENANT-ID" --value "$TENANT" --vault-name $KV_NAME
+
+# Output instructions
+echo "Project Ready"
+echo "Store this information into your .envrc file for Terraform:"
+echo '"export ARM_CLIENT_ID=$(az keyvault secret show --name ARM-CLIENT-ID --vault-name '$KV_NAME' --query value -o tsv)"'
+echo '"export ARM_CLIENT_SECRET=$(az keyvault secret show --name ARM-CLIENT-SECRET --vault-name '$KV_NAME' --query value -o tsv)"'
+echo '"export ARM_SUBSCRIPTION_ID=$(az keyvault secret show --name ARM-SUBSCRIPTION-ID --vault-name '$KV_NAME' --query value -o tsv)"'
+echo '"export ARM_TENANT_ID=$(az keyvault secret show --name ARM-TENANT-ID --vault-name '$KV_NAME' --query value -o tsv)"'
