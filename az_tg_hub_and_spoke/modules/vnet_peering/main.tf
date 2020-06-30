7c4afb14c9d51de7b44098544c4474182eb93bf8
ang <andy.ng@digicatapult.org.uk>
2020-06-30 13:00:05 +0100

    vnet peering module data type

--- az_tg_hub_and_spoke/modules/vnet_peering/main.tf
+++ az_tg_hub_and_spoke/modules/vnet_peering/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network_peering" "hub-spoke-peer" {
   name                         = "${var.hub_vnet_prefix}-to-${var.spoke_vnet_prefix}-peer"
   resource_group_name          = "${var.rg_prefix}-rg"
-  virtual_network_name         = data.virtual_network_name.hub_vnet.name
-  remote_virtual_network_id    = data.virtual_network_name.spoke_vnet.id
+  virtual_network_name         = data.azurerm_virtual_network.hub_vnet.name
+  remote_virtual_network_id    = data.azurerm_virtual_network.spoke_vnet.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
   allow_gateway_transit        = true
