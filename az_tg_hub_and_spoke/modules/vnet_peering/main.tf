3ed8ee012925fb53f008e017783998413489d9ed
ang <andy.ng@digicatapult.org.uk>
2020-06-30 13:19:08 +0100

    vnet peering both ways

--- az_tg_hub_and_spoke/modules/vnet_peering/main.tf
+++ az_tg_hub_and_spoke/modules/vnet_peering/main.tf
@@ -8,3 +8,14 @@ resource "azurerm_virtual_network_peering" "hub-spoke-peer" {
   allow_gateway_transit        = true
   use_remote_gateways          = false
 }
+
+resource "azurerm_virtual_network_peering" "spoke-hub-peer" {
+  name                         = "${var.spoke_vnet_prefix}-to-${var.hub_vnet_prefix}-peer"
+  resource_group_name          = "${var.rg_prefix}-rg"
+  virtual_network_name         = data.azurerm_virtual_network.spoke_vnet.name
+  remote_virtual_network_id    = data.azurerm_virtual_network.hub_vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = true
+  use_remote_gateways          = false
+}
